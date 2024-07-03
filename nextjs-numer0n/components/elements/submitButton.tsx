05ff5e8ce2219409bc885f8f31dcdf40e8c4796b
shion <77258886+shion-4711@users.noreply.github.com>
2024-07-04 08:25:11 +0900

    Tailwind有効化・トップページ作成

---
+++ nextjs-numer0n/components/elements/submitButton.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    text: string
+}
+
+export default function SubmitButton(props: Props) {
+    return (
+        <>
+            <button 
+                type="button"
+                className="bg-blue-700 rounded-md shadow-md text-white h-8 w-32 hover:bg-blue-600"
+            >{props.text}</button>
+        </>
+    )
+}

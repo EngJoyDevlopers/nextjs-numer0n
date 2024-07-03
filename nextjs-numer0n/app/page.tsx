2d98c5c1443ec69eb09057d9d03175bce2a06dcc
shion <77258886+shion-4711@users.noreply.github.com>
2024-07-04 08:42:16 +0900

    .ts, .tsxに対してprettier導入(jsxは未対応)

--- nextjs-numer0n/app/page.tsx
+++ nextjs-numer0n/app/page.tsx
@@ -1,10 +1,10 @@
-import SubmitButton from '@/components/elements/submitButton';
+import SubmitButton from '@/components/elements/submitButton'
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6 grid place-items-center">
-        <h1 className="text-6xl w-full text-center">Numer0n</h1>
-          <SubmitButton text="START"></SubmitButton>
+      <h1 className="text-6xl w-full text-center">Numer0n</h1>
+      <SubmitButton text="START"></SubmitButton>
     </main>
-  );
+  )
 }
--- nextjs-numer0n/components/elements/submitButton.tsx
+++ nextjs-numer0n/components/elements/submitButton.tsx
@@ -1,14 +1,16 @@
 type Props = {
-    text: string
+  text: string
 }
 
 export default function SubmitButton(props: Props) {
-    return (
-        <>
-            <button 
-                type="button"
-                className="bg-blue-700 rounded-md shadow-md text-white h-8 w-32 hover:bg-blue-600"
-            >{props.text}</button>
-        </>
-    )
+  return (
+    <>
+      <button
+        type="button"
+        className="bg-blue-700 rounded-md shadow-md text-white h-8 w-32 hover:bg-blue-600"
+      >
+        {props.text}
+      </button>
+    </>
+  )
 }
